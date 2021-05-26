f8f3c94d921abee99d7c1ac69060dc8b636b8846
Riesta Pinky Nurul Arifah <0320200022@polman.astra.ac.id>
2021-05-26 18:50:19 +0700

    Coba di form hapus jabatan

--- BekerjaDenganData2_22/FormHapusJabatan.Designer.cs
+++ BekerjaDenganData2_22/FormHapusJabatan.Designer.cs
@@ -77,7 +77,7 @@
             // 
             // txtIdJabatan
             // 
-            this.txtIdJabatan.Location = new System.Drawing.Point(126, 99);
+            this.txtIdJabatan.Location = new System.Drawing.Point(110, 63);
             this.txtIdJabatan.Name = "txtIdJabatan";
             this.txtIdJabatan.Size = new System.Drawing.Size(104, 20);
             this.txtIdJabatan.TabIndex = 18;
@@ -104,11 +104,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(122, 30);
+            this.label1.Location = new System.Drawing.Point(195, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.Size = new System.Drawing.Size(110, 20);
             this.label1.TabIndex = 15;
-            this.label1.Text = "Jabatan";
+            this.label1.Text = "Masuk Gaaa";
             // 
             // FormHapusJabatan
             // 
